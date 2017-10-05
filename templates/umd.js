dd8fab83d1adfb829529e92200f42ef37d76a5c8
Maurizio Lupo <maurizio.lupo@gmail.com>
2017-10-05 16:49:05 +0100

    adding importExternal

--- templates/umd.js
+++ templates/umd.js
@@ -1,6 +1,8 @@
 if (typeof exports === 'object'){
+  define.importExternal = importExternal;
   module.exports = define;
 }
 else{
   this.asyncDefine = define;
+  this.importExternal = importExternal;
 }
